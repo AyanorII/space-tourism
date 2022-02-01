65c98987b17feba0e417a1db86733bd851dd6ade
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-02-01 21:50:45 +0900

    move assets folder

--- space-tourism/src/App.js
+++ space-tourism/src/App.js
@@ -1,10 +1,17 @@
-// TODO: Import dependencies
-// TODO: Reset CSS
-// TODO: Set styles
+// Dependencies:
+import GlobalStyle from "./styles/globalStyles";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+// Components:
+import Home from "./pages/Home/Home";
+import Theme from "./styles/Theme";
+
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Theme>
+      <GlobalStyle />
+        <Home />
+    </Theme>
   );
 }
 
